6fbc6b6f19f70cf720cce42bbb048a32a11344fa
Will King <contact@wking.dev>
2024-04-11 15:43:55 -0500

    WIP: Member page

--- app/routes/$groupId+/members.tsx
+++ app/routes/$groupId+/members.tsx
@@ -1,3 +1,80 @@
+import { invariant } from '@epic-web/invariant'
+import { type LoaderFunctionArgs } from '@remix-run/node'
+import { json, useLoaderData } from '@remix-run/react'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '#app/components/ui/table.js'
+import { prisma } from '#app/utils/db.server.js'
+
+export async function loader({ params }: LoaderFunctionArgs) {
+	const { groupId } = params
+	invariant(groupId, 'Missing Group ID.')
+
+	const members = await prisma.group
+		.findUniqueOrThrow({
+			where: { id: groupId },
+			select: {
+				members: {
+					select: {
+						id: true,
+						roles: true,
+						account: {
+							select: {
+								handle: true,
+								email: true,
+								name: true,
+							},
+						},
+					},
+				},
+			},
+		})
+		.then(({ members }) =>
+			members.map(member => ({
+				id: member.id,
+				name: member.account.name,
+				email: member.account.email,
+				handle: member.account.handle,
+				roles: member.roles.map(({ name }) => name),
+			})),
+		)
+
+	return json({
+		members,
+	})
+}
+
 export default function Screen() {
-	return <div>Invites</div>
+	const { members } = useLoaderData<typeof loader>()
+
+	return (
+		<div>
+			<Table dense>
+				<TableHead>
+					<TableRow>
+						<TableHeader>Name</TableHeader>
+						<TableHeader>Email</TableHeader>
+						<TableHeader>Role</TableHeader>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{members.map(member => (
+						<TableRow key={member.handle}>
+							<TableCell className="font-medium">{member.name}</TableCell>
+							<TableCell>{member.email}</TableCell>
+							<TableCell className="text-zinc-500">
+								{member.roles.join(' - ')}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+			<button>Invite New Member</button>
+		</div>
+	)
 }
